5614e517515cc34b536e740fa60dbfb94544e831
Datrixx <datrixxacc@gmail.com>
2025-06-16 14:03:31 -0400

    fix images rutes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://danielvflores.github.io',
-    base: 'danielvflores-portfolio',
+    base: '/danielvflores-portfolio/',
 });
